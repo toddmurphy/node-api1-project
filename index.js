3b89369783cdcbaa572e4f90580fa6f7eabfa24c
toddmurphy <toddmurphy93@gmail.com>
2020-01-13 16:40:51 -0400

    app structure setup, server running, ready to start making endpoints

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+const port = 8000;
+
+//write following endpoint: GET (all users), GET (user by id), POST, DELETE, PUT
+
+//GET --> all user
+
+//POST -> create a new user
+
+//DELETE --> delete a user by ID
+
+//GET --> single user by ID
+
+//PUT --> update a user by ID
+
+server.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});

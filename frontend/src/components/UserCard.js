aae5ff6b7d7da6b8accc8c0348ad08e71e98569e
toddmurphy <toddmurphy93@gmail.com>
2020-01-13 20:20:33 -0400

    working on delete but getting error on front end

--- frontend/src/components/UserCard.js
+++ frontend/src/components/UserCard.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const UserCard = () => {
+const UserCard = props => {
+  console.log(props);
+  const { name, bio, id } = props.user;
 
-    
-    return ( 
-        <div>
-            <h3>User card</h3>
-            <button>Edit</button>
-            <button>Delete</button>
-        </div>
-     );
-}
- 
+  //handleDelete
+  const handleDelete = event => {
+    event.preventDefault();
+
+    axios
+      .delete(`http://localhost:8000/api/users/${id}`)
+      .then(res => {
+        console.log(res);
+        const filteredData = props.userData.filter(user => user.id !== id);
+        props.setUserData(filteredData);
+      })
+      .catch(error => {
+        console.log('Sorry, no user deleted', error);
+      });
+  };
+
+  //handleEdit
+
+  return (
+    <div>
+      <p>{id}</p>
+      <p>{name}</p>
+      <p>{bio}</p>
+      <button>Edit</button>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
+
 export default UserCard;
--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,28 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import UserCard from './UserCard';
 import axios from 'axios';
 
 const UserList = () => {
-    const [userData, setUserData] = useState([])
+  const [userData, setUserData] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/api/users')
-            .then(res => {
-                console.log(res)
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/users')
+      .then(res => {
+        console.log(res.data);
 
-                // setUserData(res)
-            })
-            .catch(error => {
-                console.log('No user data returned', error)
-            })
-    }, [])
+        setUserData(res.data);
+      })
+      .catch(error => {
+        console.log('No user data returned', error);
+      });
+  }, []);
 
+  return (
+    <div>
+      {userData.map((user, index) => {
+        return <UserCard key={index} user={user} userData={userData} setUserData={setUserData} />;
+      })}
+    </div>
+  );
+};
 
-    return ( 
-        <div>
-            <UserCard/>
-        </div>
-     );
-}
- 
 export default UserList;
